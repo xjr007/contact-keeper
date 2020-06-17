bb6708d0c22038b7ec62fb241d6483a29775d5e6
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-17 19:51:46 +0200

    vid 13

--- routes/contacts.js
+++ routes/contacts.js
@@ -25,16 +25,68 @@ router.get('/', auth, async (req, res) => {
 // @desc    Add new contact
 // @access  Private
 
-router.post('/', (req, res) => {
-    res.send('Add contact');
+router.post('/', [auth, [
+    check('name', 'Name is required').not().isEmpty()
+] ], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {name, email, phone, type} = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   Put api/contacts/:id
 // @desc    Update contact
 // @access  Private
 
-router.put('/:id', (req, res) => {
-    res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+    const {name, email, phone, type} = req.body;
+
+    //Build a contact object
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorised' });
+        }
+
+        contact = await Contact.findByIdAndUpdate(
+            req.params.id,
+            { $set: contactFields },
+            { new: true }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
 });
 
 // @route   DELETE api/contacts/:id
