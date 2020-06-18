ed69f273be6f0e33ba0eb0b9726492ff32156f8d
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-18 10:24:55 +0200

    Backend finshed

--- routes/contacts.js
+++ routes/contacts.js
@@ -82,10 +82,11 @@ router.put('/:id', auth, async (req, res) => {
             { $set: contactFields },
             { new: true }
         );
+        res.json(contact);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
-    }
+    };
 
 });
 
@@ -93,8 +94,25 @@ router.put('/:id', auth, async (req, res) => {
 // @desc    Delete contact
 // @access  Private
 
-router.delete('/:id', (req, res) => {
-    res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorised' });
+        }
+
+        await Contact.findByIdAndRemove(req.params.id); 
+
+        res.json({ msg: 'Contact removed' });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    };
 });
 
 module.exports = router;
