bc18f962815c3598e22a626908c4aa3bdeea8679
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-23 10:07:12 +0200

    alerts created

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,12 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+
 
 import './App.css';
 
@@ -17,18 +20,21 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch />
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/about' component={About}/>
-              <Route exact path='/register' component={Register}/>
-              <Route exact path='/login' component={Login}/>
-          </div>
-        </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Alerts />
+                <Switch />
+                  <Route exact path='/' component={Home}/>
+                  <Route exact path='/about' component={About}/>
+                  <Route exact path='/register' component={Register}/>
+                  <Route exact path='/login' component={Login}/>
+            </div>
+          </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
